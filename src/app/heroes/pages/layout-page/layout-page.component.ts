1b77766b24f7d1fc5d2ce12664ccb6174a6a56a3
EdsonEvangelista <yokarc1989@gmail.com>
2023-09-28 17:34:34 -0500

    Casi no hice nada hoy

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -4,6 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
   styles: [
+    `.logout-container {
+      display: flex;
+      align-items: center; /* Vertically center items */
+    }
+
+    .profile-image {
+      margin-left: 10px; /* Add spacing between the "Logout" button and the image */
+    }`
   ]
 })
 export class LayoutPageComponent {
